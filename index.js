67293f2991f3bb2f8bf02295149baafb47acd8f6
Nasha <natalif158@gmail.com>
2023-06-12 10:18:04 -0300

    comienzo del proyecto

---
+++ index.js
@@ -0,0 +1,59 @@
+// JSON.parce
+
+// const jsonStr = '{"name": "John", "age": 30, "city": "New York"}';
+// const obj = JSON.parse(jsonStr);
+// console.log(obj.name); // Output: John
+// console.log(obj.age); // Output: 30
+// console.log(obj.city); // Output: New York
+
+// JSON.stringify
+
+// const obj = { name: "John", age: 30, city: "New York" };
+// const jsonStr = JSON.stringify(obj);
+// console.log(jsonStr); // Output: {"name":"John","age":30,"city":"New York"}
+
+//Manejo de JSON, agregar nuevos valores y modificar
+
+// const jsonStr = '{"name": "John", "age": 30, "city": "New York"}';
+// const obj = JSON.parse(jsonStr);
+
+// obj.age = 31; // Modificar la propiedad "age"
+// obj.country = "USA"; // Agregar una nueva propiedad
+
+// const updatedJsonStr = JSON.stringify(obj);
+// console.log(updatedJsonStr);
+
+
+//Find ejemplo:
+
+// const numbers = [1, 2, 3, 4, 5];
+// const evenNumber = numbers.find((number) => number % 2 === 0);
+
+// console.log(evenNumber); // Output: 2
+
+//Filter
+
+// const numbers = [1, 2, 3, 4, 5];
+// const evenNumbers = numbers.filter((number) => number % 2 === 0);
+
+// console.log(evenNumbers); // Output: [2, 4]
+
+//Map 
+
+// const numbers = [1, 2, 3, 4, 5];
+// const squaredNumbers = numbers.map((number) => number * number);
+
+// console.log(squaredNumbers); // Output: [1, 4, 9, 16, 25]
+
+//Reduce
+
+// const numbers = [1, 2, 3, 4, 5];
+// const sum = numbers.reduce((accumulator, current) => accumulator + current, 0);
+
+// console.log(sum); // Output: 15
+
+
+const colores = ['rojo', 'verde', 'azul', 'amarillo', 'naranja', 'rosa', 'violeta', 'rojo', 'verde', 'azul', ];
+
+// un ejemplo de un array con muchos colores y quiero filtrarlo dependiendo de lo que el usuario escriba en un input con javaScript
+
